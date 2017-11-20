e5e3d9cab406d315cb9e47268e6bd723f1598ee6
jaythaceo <jaythaceo@gmail.com>
2017-11-20 17:29:40 -0500

    [chore] Added merge_sort

---
+++ src/merge_sort_demo.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "generic.h"
+#include "merge_sort.h"
+
+using namespace alg;
+
+int main(int argc, char const *argv[])
+{
+	const int MAX_ELEMENTS = 20;
+	
+	return 0;
+}
